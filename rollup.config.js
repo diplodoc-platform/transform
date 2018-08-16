57e4da956a3a08c59171664f188a1d0cd35dbb53
Andrey Morozov <amje@yandex-team.ru>
2018-08-17 00:19:25 +0300

    feat: tabs

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+'use strict';
+const babel = require('rollup-plugin-babel');
+
+module.exports = {
+    input: 'src/js/index.js',
+    output: {
+        file: 'dist/js/yfm.js',
+        format: 'umd',
+        name: 'yfm'
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            externalHelpersWhitelist: [
+                'defineProperties',
+                'createClass',
+                'inheritsLoose',
+                'defineProperty',
+                'objectSpread'
+            ]
+        })
+    ]
+};
---
+++ src/js/tabs.js
@@ -0,0 +1,56 @@
+const Selector = {
+    TABS: '.yfm .tabs',
+    TAB_LIST: '.yfm .tab-list',
+    TAB: '.yfm .tab',
+    TAB_PANEL: '.yfm .tab-panel'
+};
+
+const ClassName = {
+    ACTIVE: 'active'
+};
+
+function selectTab(element) {
+    if (
+        !element.parentNode ||
+        !element.parentNode.matches(Selector.TAB_LIST) ||
+        !element.parentNode.parentNode ||
+        !element.parentNode.parentNode.matches(Selector.TABS) ||
+        element.classList.contains(ClassName.ACTIVE)
+    ) {
+        return;
+    }
+
+    const tab = element;
+    const tabList = tab.parentNode;
+    const tabsContainer = tabList.parentNode;
+    const allTabs = Array.from(tabsContainer.querySelectorAll(Selector.TAB));
+    const allPanels = Array.from(tabsContainer.querySelectorAll(Selector.TAB_PANEL));
+    const targetIndex = allTabs.indexOf(tab);
+
+    for (let i = 0; i < allTabs.length; i++) {
+        const tab = allTabs[i];
+        const panel = allPanels[i];
+
+        if (i === targetIndex) {
+            tab.classList.toggle(ClassName.ACTIVE, true);
+            tab.setAttribute('aria-selected', true);
+            tab.setAttribute('tabindex', 0);
+            panel.classList.toggle(ClassName.ACTIVE, true);
+        } else {
+            tab.classList.toggle(ClassName.ACTIVE, false);
+            tab.setAttribute('aria-selected', false);
+            tab.setAttribute('tabindex', -1);
+            panel.classList.toggle(ClassName.ACTIVE, false);
+        }
+    }
+
+
+}
+
+document.addEventListener('click', (event) => {
+    if (!event.target.matches(Selector.TAB)) {
+        return;
+    }
+
+    selectTab(event.target);
+});
