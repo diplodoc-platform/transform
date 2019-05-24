2ea2a9938d02678fa9960e06dc09f089d5deec19
euspenskiy <euspenskiy@yandex-team.ru>
2019-05-24 16:48:26 +0300

    feat: Update syntax for notes CLOUDFRONT-1622

--- test/alerts.test.js
+++ test/alerts.test.js
@@ -2,7 +2,7 @@
 
 const alerts = require('../lib/plugins/alerts');
 const {callPlugin, tokenize} = require('./utils');
-const {base} = require('./data/alerts');
+const {base, customTitle, emptyTitle} = require('./data/alerts');
 
 const callAlertsPlugin = callPlugin.bind(null, alerts);
 
@@ -11,11 +11,11 @@ describe('Alerts', () => {
         const result = callAlertsPlugin(tokenize([
             'Text before',
             '',
-            '{% alert warning %}',
+            '{% note info %}',
             '',
             'Текст примечания.',
             '',
-            '{% endalert %}',
+            '{% endnote %}',
             '',
             'Text after'
         ]), {});
@@ -25,23 +25,56 @@ describe('Alerts', () => {
 
     describe('Should support all types', () => {
         [
-            {type: 'note', title: 'Примечание'},
+            {type: 'info', title: 'Примечание'},
             {type: 'tip', title: 'Совет'},
-            {type: 'warning', title: 'Предупреждение'},
-            {type: 'important', title: 'Важная информация'}
+            {type: 'alert', title: 'Предупреждение'},
+            {type: 'important', title: 'Важная информация'},
+            {type: 'error', title: 'Ошибка'}
         ].forEach(({type, title}) => {
             test(`should support type: ${type}`, () => {
                 const result = callAlertsPlugin(tokenize([
-                    `{% alert ${type} %}`,
+                    `{% note ${type} %}`,
                     '',
                     'Текст примечания.',
                     '',
-                    '{% endalert %}'
+                    '{% endnote %}'
                 ]), {});
 
-                expect(result[0].attrs[0][1]).toEqual(`alert ${type}`);
+                expect(result[0].attrs[0][1]).toEqual(`note ${type}`);
                 expect(result[2].children[1].content).toEqual(title);
             });
         });
+    });
+
+    test('Should support custom title', () => {
+        const result = callAlertsPlugin(tokenize([
+            'Text before',
+            '',
+            '{% note info "Custom title" %}',
+            '',
+            'Текст примечания.',
+            '',
+            '{% endnote %}',
+            '',
+            'Text after'
+        ]), {});
+
+        expect(result).toEqual(customTitle);
+    });
+
+    test('Should support empty title', () => {
+        const result = callAlertsPlugin(tokenize([
+            'Text before',
+            '',
+            '{% note info "" %}',
+            '',
+            'Текст примечания.',
+            '',
+            '{% endnote %}',
+            '',
+            'Text after'
+        ]), {});
+
+        expect(result).toEqual(emptyTitle);
     });
 });
