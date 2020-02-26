077bf520629f0fc9706ba87cd3ab95a3e5a63819
daff <daff@yandex-team.ru>
2020-02-26 18:00:17 +0300

    feat: add Cut CLOUDFRONT-1917

---
+++ src/js/cut.js
@@ -0,0 +1,24 @@
+const Selector = {
+    CUT: '.yfm .yfm-cut',
+    TITLE: '.yfm .yfm-cut-title',
+    CONTENT: '.yfm .yfm-cut-content',
+};
+
+const ClassName = {
+    OPEN: 'open',
+};
+
+function toggleCut(element) {
+    const cutEl = element.parentNode;
+    cutEl.classList.toggle(ClassName.OPEN);
+}
+
+if (typeof document !== 'undefined') {
+    document.addEventListener('click', (event) => {
+        if (!event.target.matches(Selector.TITLE)) {
+            return;
+        }
+
+        toggleCut(event.target);
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import './polyfill';
 import './tabs';
 import './code';
+import './cut';
