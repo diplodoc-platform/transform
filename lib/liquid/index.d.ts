ff02ee62a85e39547ab5b058dcbcf38fada0cab6
Vladimir Fedin <vladimirfedin@yandex-team.ru>
2020-05-13 12:54:51 +0300

    Reverted settings section for liquid

--- lib/liquid/index.d.ts
+++ lib/liquid/index.d.ts
@@ -1,5 +1,9 @@
 export default function liquid(
     originInput: string,
     vars: Record<string, string>,
-    path: string
+    path: string,
+    settings?: {
+        conditions?: boolean;
+        substitutions?: boolean;
+    }
 ): string;
