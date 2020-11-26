38e92b0f853764bd4ed38f5440f92e6ad17852c1
birman111 <birman111@yandex-team.ru>
2020-11-26 09:56:43 +0300

    DOCSTOOLS-336: add support for single page in plugins in yfm-docs
    Change links, anchors, paths to includes and assets if the oneFile
    option is passed in the options.

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,3 +1,5 @@
 export function resolveRelativePath(fromPath: string, relativePath: string): string;
 
 export function isLocalUrl(path?: string): string;
+
+export function getSinglePageAnchorId(options: {root: string; currentPath: string, pathname?:string, hash?: string}): string;
