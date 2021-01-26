9592baf1db04a52e017e1dea09987de3564849dd
burashka <burashka-lcme@yandex.ru>
2021-01-26 19:07:11 +0300

    Split utils and collect/plugins CLOUDFRONT-3973

--- lib/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export function resolveRelativePath(fromPath: string, relativePath: string): string;
-
-export function isLocalUrl(path?: string): string;
-
-export function getSinglePageAnchorId(options: {root: string; currentPath: string, pathname?:string, hash?: string}): string;
