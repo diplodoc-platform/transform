045421603f518fa775e5d04c3e38ad26677d8fc8
birman111 <birman111@yandex-team.ru>
2021-03-10 14:48:57 +0300

    refactor: address review feedback

--- lib/liquid/utils.d.ts
+++ lib/liquid/utils.d.ts
@@ -1,1 +1,11 @@
 export function removeIndentBlock(str: string): string;
+
+export function getPreparedLeftContent({
+    content,
+    tagStartPos,
+    tagContent
+}: {
+    content: string;
+    tagStartPos: number,
+    tagContent: string
+}): string;
