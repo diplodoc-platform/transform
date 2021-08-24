32c52365b6801a3fb3b1bc42824ea75a81d3bb41
birman111 <birman111@yandex-team.ru>
2021-08-24 18:45:13 +0300

    refactor: address review feedback

--- test/markdownlint-custom-rules/liquidSourceMap.test.js
+++ test/markdownlint-custom-rules/liquidSourceMap.test.js
@@ -36,4 +36,52 @@ describe('Check source map after liquid', () => {
 
         expect(sourceMap).toEqual({'1': '1', '2': '3', '5': '6', '6': '8'});
     });
+
+    it('Should works with two conditions in a row', () => {
+        const input =
+            /*1*/ '{% if test %}\n' +
+            /*2*/ '    How are you?\n' +
+            /*3*/ '{% endif %}\n' +
+            /*4*/ '{% if test %}\n' +
+            /*5*/ '    How are you?\n' +
+            /*6*/ '{% endif %}';
+
+        const {sourceMap} = liquid(input, vars, testFn, {withSourceMap: true});
+
+        expect(sourceMap).toEqual({'1': '2', '2': '5'});
+    });
+
+    it('Should works with nested conditions', () => {
+        const input =
+            /*1*/ 'Prefix\n' +
+            /*2*/ '{% for user in users %}\n' +
+            /*3*/ '{{user}}\n' +
+            /*4*/ '{% endfor %}\n' +
+            /*5*/ '{% if test %}\n' +
+            /*6*/ '{% if lie %}\n' +
+            /*7*/ 'How are you 2?\n' +
+            /*8*/ '{% endif %}\n' +
+            /*9*/ 'How are you?\n' +
+            /*10*/ '{% endif %}\n' +
+            /*11*/ 'Postfix';
+
+        const {sourceMap} = liquid(input, vars, testFn, {withSourceMap: true});
+
+        expect(sourceMap).toEqual({'1': '1', '2': '3', '5': '9', '6': '11'});
+    });
+
+    it('Should works with nested cycles', () => {
+        const input =
+            /*1*/ 'Prefix\n' +
+            /*2*/ '{% for user1 in users %}\n' +
+            /*3*/ '{% for user2 in users %}\n' +
+            /*4*/ '{{user1}}+{{user2}}\n' +
+            /*5*/ '{% endfor %}\n' +
+            /*6*/ '{% endfor %}\n' +
+            /*7*/ 'Postfix';
+
+        const {sourceMap} = liquid(input, vars, testFn, {withSourceMap: true});
+
+        expect(sourceMap).toEqual({'1': '1', '4': '5', '11': '7'});
+    });
 });
