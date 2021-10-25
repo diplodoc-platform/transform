3a130d28705bc065cf210f5b2f37d00fee5a7171
birman111 <birman111@yandex-team.ru>
2021-10-25 07:10:18 +0300

    fix: add missing highlight.js class

--- test/table.test.js
+++ test/table.test.js
@@ -254,7 +254,7 @@ describe('Table plugin', () => {
             '<tr>\n' +
             '<td>1</td>\n' +
             '<td>\n' +
-            '<pre><code>|===\n' +
+            '<pre><code class="hljs">|===\n' +
             '| 4\n' +
             '| 5\n' +
             '\n' +
