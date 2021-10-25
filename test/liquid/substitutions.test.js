9a09147922e1b7569b585d8ecddf822426ccf613
birman111 <birman111@yandex-team.ru>
2021-10-25 07:07:56 +0300

    DOCSTOOLS-818: support keepNotVar setting

--- test/liquid/substitutions.test.js
+++ test/liquid/substitutions.test.js
@@ -1,14 +1,24 @@
-const substitutions = require('../../lib/liquid/substitutions');
+const liquid = require('../../lib/liquid');
 
 describe('Substitutions', () => {
     test('Should substitute to inline text', () => {
         expect(
-            substitutions('Hello {{ user.name }}!', {user: {name: 'Alice'}}),
+            liquid('Hello {{ user.name }}!', {user: {name: 'Alice'}}),
         ).toEqual('Hello Alice!');
     });
     test('Should not substitute variables start with dot', () => {
         expect(
-            substitutions('Hello {{ .name }}'),
+            liquid('Hello {{ .name }}'),
         ).toEqual('Hello {{ .name }}');
+    });
+    test('Should not substitute variables wrapped not_var', () => {
+        expect(
+            liquid('Hello not_var{{ user.name }}!', {user: {name: 'Alice'}}),
+        ).toEqual('Hello {{ user.name }}!');
+    });
+    test('Keep not_var syntax', () => {
+        expect(
+            liquid('Hello not_var{{ user.name }}!', {user: {name: 'Alice'}}, '', {keepNotVar: true}),
+        ).toEqual('Hello not_var{{ user.name }}!');
     });
 });
