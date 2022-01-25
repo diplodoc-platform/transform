622ad4684b2a307566c27142d631dcae026fd09b
Max Travintsev <cjmixer2@gmail.com>
2022-01-25 11:42:31 +0300

    feat: custom markdown-it typings

---
+++ src/transform/typings.ts
@@ -0,0 +1,7 @@
+import DefaultMarkdownIt from 'markdown-it';
+import DefaultStateCore from 'markdown-it/lib/rules_core/state_core';
+
+export interface MarkdownIt extends DefaultMarkdownIt { assets?: string[], meta?: string[] };
+export interface StateCore extends DefaultStateCore {
+    md: MarkdownIt;
+}
