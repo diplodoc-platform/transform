276286e3d6450e80d813c7fa37e0d6a07fcf4f05
martyanov-av <martyanov-av@yandex-team.ru>
2022-09-13 16:22:18 +0300

    revert extractTitle fn changes

--- src/transform/title.ts
+++ src/transform/title.ts
@@ -6,19 +6,11 @@ export = function extractTitle(tokens: Token[]) {
         titleTokens: Token[] = [];
 
     if (Array.isArray(tokens) && tokens.length > 0) {
-        let start = 0;
-        const skipTokenType = ['__yfm_lint'];
-
-        while (skipTokenType.includes(tokens[start].type)) {
-            start++;
+        if (tokens[0].type === 'heading_open' && tokens[0].tag === 'h1') {
+            titleTokens = tokens[1].children || [];
+            title = tokens[1].content;
         }
-
-        if (tokens[start].type === 'heading_open' && tokens[start].tag === 'h1') {
-            titleTokens = tokens[start + 1].children || [];
-            title = tokens[start + 1].content;
-            // cut out "heading_open", "inline" and "heading_close" tokens
-            contentTokens = [...tokens.slice(0, start), ...tokens.slice(start + 3)];
-        }
+        contentTokens = tokens.slice(3);
     }
 
     return {
