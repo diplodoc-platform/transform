dbb574cd5bc224195e5b6cf85cce8312c5af15bb
3y3k0 <3y3k0@yandex-team.ru>
2023-03-23 13:12:21 +0300

    feat(md): Refactor transform index

---
+++ src/transform/plugins.ts
@@ -0,0 +1,35 @@
+import type {MarkdownItPluginCb} from './plugins/typings';
+
+import meta from './plugins/meta';
+import deflist from './plugins/deflist';
+import cut from './plugins/cut';
+import notes from './plugins/notes';
+import anchors from './plugins/anchors';
+import tabs from './plugins/tabs';
+import code from './plugins/code';
+import sup from './plugins/sup';
+import video from './plugins/video';
+import monospace from './plugins/monospace';
+import yfmTable from './plugins/table';
+import file from './plugins/file';
+import imsize from './plugins/imsize';
+import term from './plugins/term';
+
+const defaultPlugins = [
+    meta,
+    deflist,
+    cut,
+    notes,
+    anchors,
+    tabs,
+    code,
+    sup,
+    video,
+    monospace,
+    yfmTable,
+    file,
+    imsize,
+    term,
+] as MarkdownItPluginCb[];
+
+export = defaultPlugins;
