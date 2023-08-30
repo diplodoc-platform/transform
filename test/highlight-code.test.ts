d3c1869ded30cd0d62a29c86c324f1e0a7a4a4f5
Alexey <sudilovskiy@yandex-team.ru>
2023-08-30 17:01:13 +0300

    feat: add `highlight.js@11` to `peerDependencies` (#294)

---
+++ test/highlight-code.test.ts
@@ -0,0 +1,20 @@
+import {dirname} from 'path';
+import transform from '../src/transform';
+
+const mocksPath = require.resolve('./utils.ts');
+const transformYfm = (text: string) => {
+    const {
+        result: {html},
+    } = transform(text, {
+        plugins: [],
+        path: mocksPath,
+        root: dirname(mocksPath),
+    });
+    return html;
+};
+
+test('should highlight code', () => {
+    const result = transformYfm("```ts\nconst x: string = 'y';\n```");
+
+    expect(result).toMatchSnapshot();
+});
