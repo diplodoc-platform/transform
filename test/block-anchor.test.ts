1de722333b05f5b7ec1ed97237089b2bab86c523
moki <morozov.kirill.moki@gmail.com>
2023-11-30 13:12:46 +0300

    test(plugins/block-anchors): refactor

--- test/block-anchor.test.ts
+++ test/block-anchor.test.ts
@@ -1,53 +1,45 @@
-import transform from '../src/transform';
+import initMarkdown from '../src/transform/md';
 import plugin from '../src/transform/plugins/block-anchor';
 import anchorsPlugin from '../src/transform/plugins/anchors';
 
-let transformYfm = (text: string) => {
-    const {
-        result: {html},
-    } = transform(text, {
-        plugins: [plugin],
-    });
-    return html;
-};
+const {parse, compile} = initMarkdown({plugins: [plugin, anchorsPlugin]});
 
 describe('block-anchor', function () {
-    describe('simple rendering', () => {
-        it('should render an hr tag', () => {
-            expect(transformYfm('{%anchor my-anchor%}')).toBe(
-                '<hr id="my-anchor" class="visually-hidden" />',
-            );
-        });
-        it('does not consume things it should not', () => {
-            expect(transformYfm('# Heading \n {%anchor my-anchor%} \n # Other heading')).toEqual(
-                '<h1 id="heading"><a href="#heading" class="yfm-anchor" aria-hidden="true">' +
-                    '<span class="visually-hidden">Heading</span></a>Heading</h1>\n' +
-                    '<hr id="my-anchor" class="visually-hidden" /><h1 id="other-heading">' +
-                    '<a href="#other-heading" class="yfm-anchor" aria-hidden="true">' +
-                    '<span class="visually-hidden">Other heading</span></a>Other heading</h1>\n',
-            );
-        });
+    it('renders block-anchor', () => {
+        const input = '{%anchor my-anchor%}';
+        const expected = '<hr id="my-anchor" class="visually-hidden" />';
+        const actual = compile(parse(input));
 
-        it('does not match an anchor if theres something right in front of it', () => {
-            expect(transformYfm('${%anchor my-anchor} Content')).toEqual(
-                '<p>${%anchor my-anchor} Content</p>\n',
-            );
-        });
+        expect(actual).toBe(expected);
     });
-    describe('with heading anchors', () => {
-        transformYfm = (text) => {
-            const {
-                result: {html},
-            } = transform(text, {plugins: [plugin, anchorsPlugin]});
-            return html;
-        };
-        it('does not conflict with heading anchors', () => {
-            expect(transformYfm('# Heading {#heading-anchor} \n {%anchor my-anchor%}')).toBe(
-                '<h1 id="heading-anchor">' +
-                    '<a href="#heading-anchor" class="yfm-anchor" aria-hidden="true">' +
-                    '<span class="visually-hidden">Heading</span></a>Heading</h1>\n' +
-                    '<hr id="my-anchor" class="visually-hidden" />',
-            );
-        });
+
+    it('parses anchors surrounded by other blocks', () => {
+        const input = '# Heading \n\n {%anchor my-anchor%} \n\n paragraph with content';
+        const actual = compile(parse(input));
+        const expected =
+            '<h1 id="heading"><a href="#heading" class="yfm-anchor" aria-hidden="true">' +
+            '<span class="visually-hidden">Heading</span></a>Heading</h1>\n' +
+            '<hr id="my-anchor" class="visually-hidden" /><p>paragraph with content</p>\n';
+        expect(actual).toEqual(expected);
+    });
+
+    it('parses block anchors', () => {
+        const input = '${%anchor my-anchor} Content';
+        const actual = compile(parse(input));
+        const expected = '<p>${%anchor my-anchor} Content</p>\n';
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('works with heading anchors', () => {
+        const input = '# Heading {#heading-anchor} \n {%anchor my-anchor%}';
+        const expected =
+            '<h1 id="heading-anchor">' +
+            '<a href="#heading-anchor" class="yfm-anchor" aria-hidden="true">' +
+            '<span class="visually-hidden">Heading</span></a>Heading</h1>\n' +
+            '<hr id="my-anchor" class="visually-hidden" />';
+        const actual = compile(parse(input));
+
+        expect(actual).toBe(expected);
     });
 });
