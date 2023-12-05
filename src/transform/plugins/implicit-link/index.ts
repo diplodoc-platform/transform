690a30472caeb8195917278bcd72823e9121366e
kravetsone <57632712+kravetsone@users.noreply.github.com>
2023-12-05 14:28:42 +0300

    feat: add implicit link plugin

---
+++ src/transform/plugins/implicit-link/index.ts
@@ -0,0 +1,11 @@
+
+import MarkdownIt from 'markdown-it';
+import { replaceTokens } from './implicit-link';
+
+const implicitLink = (md: MarkdownIt) => {
+    md.core.ruler.after('links', 'implicit-link', replaceTokens);
+
+    return md;
+};
+
+export default implicitLink;
