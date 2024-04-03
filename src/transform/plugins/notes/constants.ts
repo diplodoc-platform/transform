0298911f290fe2d51572553923927bf8b0d304b1
martyanov-av <martyanov-av@yandex-team.ru>
2024-04-03 11:37:04 +0300

    feat: add tr-TR, kk-KZ, uz-UZ, bg-BG, zh-TW, zh-CN, pt-PT, et-EE, el-GR

---
+++ src/transform/plugins/notes/constants.ts
@@ -0,0 +1,98 @@
+export const TITLES: Record<string, Record<string, string>> = {
+    ru: {
+        info: 'Примечание',
+        tip: 'Совет',
+        alert: 'Внимание',
+        warning: 'Важно',
+    },
+    en: {
+        info: 'Note',
+        tip: 'Tip',
+        alert: 'Alert',
+        warning: 'Warning',
+    },
+    ar: {
+        info: 'ملاحظة',
+        tip: 'نصيحة',
+        alert: 'انتباه',
+        warning: 'هام',
+    },
+    cs: {
+        info: 'Poznámka',
+        tip: 'Tip',
+        alert: 'Upozornění',
+        warning: 'Varování',
+    },
+    fr: {
+        info: 'Remarque',
+        tip: 'Astuce',
+        alert: 'Alerte',
+        warning: 'Avertissement',
+    },
+    es: {
+        info: 'Nota',
+        tip: 'Consejo',
+        alert: 'Alerta',
+        warning: 'Aviso',
+    },
+    he: {
+        info: 'מידע',
+        tip: 'טיפ',
+        alert: 'התראה',
+        warning: 'אזהרה',
+    },
+    bg: {
+        "info": "Забележка",
+        "tip": "Съвет",
+        "alert": "Внимание",
+        "warning": "Важно"
+    },
+    et: {
+        "info": "Märkus",
+        "tip": "Nõuanne",
+        "alert": "Tähelepanu",
+        "warning": "Tähtis"
+    },
+    el: {
+        "info": "Σημείωση",
+        "tip": "Συμβουλή",
+        "alert": "Προσοχή",
+        "warning": "Σημαντικό"
+    },
+    pt: {
+        "info": "Observação",
+        "tip": "Sugestão",
+        "alert": "Atenção",
+        "warning": "Importante"
+    },
+    zh: {
+        "info": "备注",
+        "tip": "提示",
+        "alert": "注意",
+        "warning": "重要"
+    },
+    "zh-tw": {
+        "info": "備注",
+        "tip": "提示",
+        "alert": "注意",
+        "warning": "重要"
+    },
+    kk: {
+        "info": "Ескерту",
+        "tip": "Кеңес",
+        "alert": "Назар аударыңыз",
+        "warning": "Маңызды"
+    },
+    tr: {
+        "info": "Not",
+        "tip": "Öneri",
+        "alert": "Dikkat",
+        "warning": "Önemli"
+    },
+    uz: {
+        "info": "Sharh",
+        "tip": "Maslahat",
+        "alert": "Diqqat",
+        "warning": "Bu muhim"
+    }
+};
