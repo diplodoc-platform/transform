74f5d6be1b2e756e01b9254d4afd8c4ff30022eb
Maxim Karpov <makame@mail.ru>
2024-08-14 15:40:38 +0300

    feat: add autotitle cache context

--- src/transform/typings.ts
+++ src/transform/typings.ts
@@ -10,8 +10,14 @@ export interface MarkdownIt extends DefaultMarkdownIt {
     assets?: string[];
     meta?: string[];
 }
+
 export interface StateCore extends DefaultStateCore {
     md: MarkdownIt;
+}
+
+export interface CacheContext {
+    get(key: string): string | null | undefined;
+    set(key: string, value: string | null | undefined): void;
 }
 
 export type HighlightLangMap = Record<string, LanguageFn>;
@@ -49,6 +55,7 @@ export interface OptionsType {
     transformLink?: (href: string) => string;
     getPublicPath?: (options: OptionsType, href?: string) => string;
     renderInline?: boolean;
+    cache?: CacheContext;
     [x: string]: unknown;
 }
 
