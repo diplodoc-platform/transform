8174fcce54473dbc3435765df686d5b10d40a6f8
3y3 <3y3@ya.ru>
2024-09-05 21:27:07 +0300

    chore: Add new lint infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: require.resolve('@diplodoc/lint/eslint-config'),
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+};
