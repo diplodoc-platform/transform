aa6f3915dc24ac175a29db2109e4ab667b8b4dff
Gleb Voitenko <v8tenko@gmail.com>
2024-10-21 13:51:37 +0300

    fix: bump tabs plugin v3.5.0

--- src/transform/plugins/tabs.ts
+++ src/transform/plugins/tabs.ts
@@ -1,3 +1,13 @@
 import {transform} from '@diplodoc/tabs-extension';
 
-export = transform({bundle: false});
+export = transform({
+    bundle: false,
+    features: {
+        enabledVariants: {
+            radio: true,
+            regular: true,
+            dropdown: true,
+            accordion: true,
+        },
+    },
+});
