9f209d69a1bb551bd0755dd6b48f260ba6fdd540
Yuriy Demidov <d3m1d0v@yandex-team.ru>
2024-11-20 16:23:30 +0300

    feat(file): replaced own plugin with file-extension (#572)

--- src/transform/plugins/file/index.ts
+++ src/transform/plugins/file/index.ts
@@ -1,11 +1,3 @@
-import type {PluginWithOptions} from 'markdown-it';
+import {transform} from '@diplodoc/file-extension';
 
-import {FILE_TOKEN, RULE_NAME} from './const';
-import {FileOptions, fileParser, fileRenderer} from './file';
-
-const filePlugin: PluginWithOptions<FileOptions> = (md, opts) => {
-    md.inline.ruler.push(RULE_NAME, fileParser(md, opts));
-    md.renderer.rules[FILE_TOKEN] = fileRenderer(md);
-};
-
-export = filePlugin;
+export = transform({bundle: false});
