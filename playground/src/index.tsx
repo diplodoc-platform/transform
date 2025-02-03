46ced419f6dc3da357c8cf57eac6d72ac64dd105
Daniel Kuznetsov <letux.dev@gmail.com>
2025-02-03 12:15:52 +0300

    fix: change local resolution alias to something that works with scss loader

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,13 +1,12 @@
 import {createRoot} from 'react-dom/client';
 import {ThemeProvider} from '@gravity-ui/uikit';
+import '@local/transform-dist/js/yfm.js';
+import '@local/transform-dist/css/yfm.css';
 
 import {App} from './App';
 import './index.css';
 import './styles.scss';
 import './overrides.scss';
-
-import '~transform-dist/js/yfm.js';
-import '~transform-dist/css/yfm.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container as HTMLElement);
