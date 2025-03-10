f572cb168fb16c9ddf1f11c3046253d5ea530922
Daniel Kuznetsov <letux.dev@gmail.com>
2025-03-10 15:13:12 +0300

    feat: contain counters for lists

---
+++ src/transform/plugins/ol-attr-conversion.ts
@@ -0,0 +1,21 @@
+import {MarkdownItPluginCb} from '../typings';
+
+const cssWhitelist = {
+    '--hier-list-start': true,
+};
+
+export const olAttrConversion: MarkdownItPluginCb = (md) => {
+    md.core.ruler.after('block', 'olAttrs', (state) => {
+        state.tokens.forEach((token) => {
+            const maybeStart = token.attrGet('start');
+
+            if (token.type === 'ordered_list_open' && maybeStart) {
+                token.attrSet('style', `--hier-list-start: ${Number(maybeStart) - 1};`);
+            }
+        });
+
+        state.env.additionalOptionsCssWhiteList ||= {};
+
+        Object.assign(state.env.additionalOptionsCssWhiteList, cssWhitelist);
+    });
+};
