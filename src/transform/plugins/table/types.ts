fb6ccdd29fa4a5487166c3b445b41d709e3136d6
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-07-01 17:36:50 +0300

    feat(table): ignore table splitters in inline code (under flag)

---
+++ src/transform/plugins/table/types.ts
@@ -0,0 +1,19 @@
+export type YfmTablePluginOptions = {
+    /**
+     * Sets whether to interpret the pipe symbol inside the code block as a table separator.
+     *
+     * Set to false if the fence rule is disabled in the parser.
+     *
+     * @default true
+     */
+    table_ignoreSplittersInBlockCode?: boolean;
+    // TODO: set table_ignoreSplittersInInlineCode=true by default in next major version
+    /**
+     * Sets whether to interpret the pipe symbol inside the inline code as a table separator.
+     *
+     * Set to true if the code_inline rule is enabled in the parser.
+     *
+     * @default false
+     */
+    table_ignoreSplittersInInlineCode?: boolean;
+};
