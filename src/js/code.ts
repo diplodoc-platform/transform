b6d94e298d10a9ef2c63c681d57aefe90b50d489
Vladislav Pyslar <vypyslar@yandex-team.ru>
2025-07-02 15:49:22 +0300

    feat: added pooper with success text

--- src/js/code.ts
+++ src/js/code.ts
@@ -1,7 +1,17 @@
+import {
+    Selector,
+    closeDefinition,
+    openClass,
+    setDefinitionId,
+    setDefinitionPosition,
+    setDefinitonAriaAttributes,
+    trapFocus,
+} from './term/utils';
 import {copyToClipboard, getEventTarget, isCustom} from './utils';
 
 const BUTTON_SELECTOR = '.yfm-clipboard-button';
 const INLINE_CODE = '.yfm-clipboard-inline-code';
+let timer: ReturnType<typeof setTimeout> | null = null;
 
 function notifySuccess(svgButton: HTMLElement | null) {
     if (!svgButton) {
@@ -48,6 +58,51 @@ function buttonCopyFn(target: HTMLElement) {
     });
 }
 
+function checkTimerAndClear() {
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+}
+
+function openDefinitionInlineCode(target: HTMLElement) {
+    const openedDefinition = document.getElementsByClassName('open')[0] as HTMLElement;
+
+    const termId = target.getAttribute('id');
+    const termKey = target.getAttribute('term-key');
+    const definitionElement = document.getElementById(termKey + '_element');
+
+    const isSameTerm = openedDefinition && termId === openedDefinition.getAttribute('term-id');
+    if (isSameTerm) {
+        checkTimerAndClear();
+        closeDefinition(openedDefinition);
+        return;
+    }
+
+    const isTargetDefinitionContent = target.closest(
+        [Selector.CONTENT.replace(' ', ''), openClass].join('.'),
+    );
+
+    if (openedDefinition && !isTargetDefinitionContent) {
+        checkTimerAndClear();
+        closeDefinition(openedDefinition);
+    }
+
+    if (!target.matches('.yfm-clipboard-inline-code') || !definitionElement) {
+        return;
+    }
+
+    setDefinitionId(definitionElement, target);
+    setDefinitonAriaAttributes(definitionElement, target);
+    setDefinitionPosition(definitionElement, target);
+
+    definitionElement.classList.toggle(openClass);
+
+    trapFocus(definitionElement);
+
+    return definitionElement;
+}
+
 function inlineCopyFn(target: HTMLElement) {
     const innerText = target.innerText;
 
@@ -56,7 +111,19 @@ function inlineCopyFn(target: HTMLElement) {
     }
 
     copyToClipboard(innerText).then(() => {
-        // SUCCESS
+        const definition = openDefinitionInlineCode(target);
+
+        if (!definition) {
+            return;
+        }
+
+        checkTimerAndClear();
+        timer = setTimeout(() => {
+            if (definition) {
+                closeDefinition(definition);
+            }
+            timer = null;
+        }, 5000);
     });
 }
 
--- src/transform/plugins/inline-code.ts
+++ src/transform/plugins/inline-code.ts
@@ -1,9 +1,56 @@
-import {MarkdownItPluginCb} from '../typings';
+import {MarkdownItPluginCb, StateCore} from '../typings';
+
+import {generateID} from './utils';
+
+// const REG_EX_INLINE_CODE = /`([^`]*)`/
 
 const inlineCode: MarkdownItPluginCb = (md) => {
     md.renderer.rules.code_inline = function (tokens, idx) {
-        return `<code class="yfm-clipboard-inline-code" id="snipet-${idx}">${tokens[idx].content}</code>`;
+        const id = tokens[idx].attrGet('id');
+
+        return `<code class="yfm-clipboard-inline-code" term-key="${id}" id="${id}" tabindex='0'>${tokens[idx].content}</code>`;
     };
+
+    md.core.ruler.after('inline', 'tooltip_code_inline', (state: StateCore) => {
+        const tokens = state.tokens;
+
+        for (let i = 0; i !== tokens.length; i++) {
+            const token = tokens[i];
+
+            if (token.type !== 'inline') {
+                continue;
+            }
+
+            if (!token.children || token.children.every((e) => e.type !== 'code_inline')) {
+                continue;
+            }
+
+            const child = token.children.find((e) => e.type === 'code_inline');
+
+            if (!child) {
+                return;
+            }
+
+            const id = generateID();
+            child.attrSet('id', id);
+
+            const dialog = new state.Token('dfn_open', 'dfn', 1);
+            dialog.attrSet('class', 'yfm yfm-term_dfn');
+            dialog.attrSet('id', `${id}_element`);
+            dialog.attrSet('role', 'dialog');
+            dialog.attrSet('aria-live', 'polite');
+            dialog.attrSet('aria-modal', 'true');
+
+            tokens.push(dialog);
+
+            const text = new state.Token('text', '', 0);
+            text.content = 'Успешно';
+            tokens.push(text);
+
+            const closeDialog = new state.Token('dfn_close', 'dfn', -1);
+            tokens.push(closeDialog);
+        }
+    });
 };
 
 export = inlineCode;
