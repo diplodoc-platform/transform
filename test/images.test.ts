2cd147c52e43d7a087daa4066ab3ee518a510936
Alexey Okhrimenko <ai_boy@live.ru>
2025-07-03 12:26:47 +0300

    Refine Cyrillic image handling

---
+++ test/images.test.ts
@@ -0,0 +1,34 @@
+import MarkdownIt from 'markdown-it';
+
+import transform from '../src/transform';
+import images from '../src/transform/plugins/images';
+import {log} from '../src/transform/log';
+
+describe('Images plugin', () => {
+    beforeEach(() => {
+        log.clear();
+    });
+
+    test('markdown-it encodes image src', () => {
+        const md = new MarkdownIt();
+        const tokens = md.parse('![тест](русские-символы.png)', {});
+        const src = tokens[1].children?.[0].attrGet('src');
+
+        expect(src).toBe(encodeURI('русские-символы.png'));
+    });
+
+    test('should handle external image links with cyrillic characters', () => {
+        const input = '![тест](https://example.com/русские-символы.png)';
+
+        const {
+            result: {html},
+        } = transform(input, {
+            plugins: [images],
+        });
+
+        expect(html).toEqual(
+            '<p><img src="https://example.com/%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B5-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%D1%8B.png" alt="тест" /></p>\n',
+        );
+        expect(log.isEmpty()).toEqual(true);
+    });
+});
