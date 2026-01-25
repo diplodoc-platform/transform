e26be7786441082f3873f687004a865d74375568
3y3 <3y3@ya.ru>
2026-01-26 01:32:46 +0300

    chore: Fix ghpages deployment

--- playground/scripts/pages.mjs
+++ playground/scripts/pages.mjs
@@ -7,29 +7,27 @@ import {generateHTML} from '../src/index.html.mjs';
 import {ts} from './configs.mjs';
 import {resolveFromPlaygroundRoot} from './utils.mjs';
 
-export const bundleDeployables = async (path) => {
-    const cwdResolvedPath = resolve(path);
+const cwdResolvedPath = resolve('./build');
 
-    try {
-        await rm(cwdResolvedPath, {recursive: true});
-    } catch (e) {
-        if (e instanceof Error && e.code !== 'ENOENT') {
-            throw new Error('failed to build pages');
-        }
+try {
+    await rm(cwdResolvedPath, {recursive: true});
+} catch (e) {
+    if (e instanceof Error && e.code !== 'ENOENT') {
+        throw new Error('failed to build pages');
     }
+}
 
-    await mkdir(cwdResolvedPath, {recursive: true});
+await mkdir(cwdResolvedPath, {recursive: true});
 
-    const html = generateHTML({
-        env: 'production',
-    });
+const html = generateHTML({
+    env: 'production',
+});
 
-    await writeFile(join(cwdResolvedPath, 'index.html'), html);
+await writeFile(join(cwdResolvedPath, 'index.html'), html);
 
-    await build(
-        ts({
-            entryPoints: [resolveFromPlaygroundRoot('src/index.tsx')],
-            outfile: join(cwdResolvedPath, 'index.js'),
-        }),
-    );
-};
+await build(
+    ts({
+        entryPoints: [resolveFromPlaygroundRoot('src/index.tsx')],
+        outfile: join(cwdResolvedPath, 'index.js'),
+    }),
+);
