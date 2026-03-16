1763a503d162c8a6ce03a4abc615be480f9d796a
stenin-nikita <stenin.nikita@gmail.com>
2026-03-16 12:25:46 +0300

    feat: use new tooltip implementation for inline code

--- src/js/inline-code/index.ts
+++ src/js/inline-code/index.ts
@@ -1,13 +1,17 @@
-import {copyToClipboard, getEventTarget, isCustom} from '../utils';
+import type {Lang} from 'src/transform/typings';
 
-import {INLINE_CODE, OPEN_CLASS} from './constant';
-import {
-    closeTooltip,
-    getInlineCodeByTooltip,
-    getTooltipElement,
-    setTooltipPosition,
-    tooltipWorker,
-} from './utils';
+import {createTooltipFactory} from '../tooltip';
+import {copyToClipboard, getEventTarget, isCustom} from '../utils';
+import {COPIED_LANG_TOKEN} from '../constant';
+
+import {INLINE_CODE} from './constant';
+
+const CLASS_INLINE_CODE = INLINE_CODE.replace('.', '');
+
+const tooltip = createTooltipFactory({
+    // NOTE: Add additional className for backward capability
+    additionalClassName: 'inline_code_tooltip',
+});
 
 export function inlineCopyFn(target: HTMLElement) {
     const innerText = target.innerText;
@@ -17,14 +21,18 @@ export function inlineCopyFn(target: HTMLElement) {
     }
 
     copyToClipboard(innerText).then(() => {
-        tooltipWorker(target);
+        const lang = document.documentElement.lang || 'en';
+        const tooltipText = COPIED_LANG_TOKEN[lang as Lang] ?? COPIED_LANG_TOKEN.en;
+
+        tooltip.show(target, tooltipText);
     });
 }
 
 if (typeof document !== 'undefined') {
+    tooltip.init();
+
     document.addEventListener('click', (event) => {
         const target = getEventTarget(event) as HTMLElement;
-
         const inline = target.matches(INLINE_CODE);
 
         if (isCustom(event) || !inline) {
@@ -37,45 +45,20 @@ if (typeof document !== 'undefined') {
     document.addEventListener('keydown', (event) => {
         if (event.key === 'Enter' && document.activeElement) {
             const activeElement = document.activeElement as HTMLElement;
-            const classInlineCode = INLINE_CODE.replace('.', '');
 
-            if (!activeElement.classList.contains(classInlineCode)) {
+            if (!activeElement.classList.contains(CLASS_INLINE_CODE)) {
                 return;
             }
 
-            const innerText = activeElement.innerText;
-
-            if (!innerText) {
-                return;
-            }
-
-            copyToClipboard(innerText).then(() => {
-                tooltipWorker(activeElement);
-            });
+            inlineCopyFn(activeElement);
         }
 
-        const inlineTooltip = getTooltipElement();
+        if (event.key === 'Escape' && tooltip.visible) {
+            const reference = tooltip.getActiveReference();
 
-        if (event.key === 'Escape' && inlineTooltip) {
-            closeTooltip(inlineTooltip);
-            getInlineCodeByTooltip(inlineTooltip)?.focus(); // Set focus back to open button after closing popup
+            tooltip.hide();
+            // Set focus back to open button after closing popup
+            reference?.focus();
         }
-    });
-
-    window.addEventListener('resize', () => {
-        const inlineTooltip = getTooltipElement();
-        if (!inlineTooltip) {
-            return;
-        }
-
-        const inlineId = inlineTooltip.getAttribute('inline-id') || '';
-        const inlineCodeElement = document.getElementById(inlineId);
-
-        if (!inlineCodeElement) {
-            inlineTooltip.classList.toggle(OPEN_CLASS);
-            return;
-        }
-
-        setTooltipPosition(inlineTooltip, inlineCodeElement);
     });
 }
